81b2c6428e92de780cef887bbe61474a727ba9a9
Jeff George <webdevjeffus@gmail.com>
2017-02-09 17:30:50 -0500

    Set up postcss pipeline in gulp.task(styles)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
 
 
 gulp.task("default", function(){
@@ -12,6 +16,7 @@ gulp.task("html", function() {
 
 gulp.task("styles", function() {
   return gulp.src("./app/assets/styles/styles.css")
+    .pipe(postcss([cssvars, nested, autoprefixer]))
     .pipe(gulp.dest("./app/temp/styles")
     );
 });

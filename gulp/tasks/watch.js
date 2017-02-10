358c35c763bce72b04fe0123a0ba0f2d54e9049d
Jeff George <webdevjeffus@gmail.com>
2017-02-10 07:54:59 -0500

    Refactor gulpfile into separate gulp task files

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var browserSync = require("browser-sync").create();
+
+gulp.task("watch", function() {
+
+  browserSync.init({
+    notify: false, /* optional--turns off msgs of changes in browser window */
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch("./app/index.html", function() {
+    browserSync.reload();
+  });
+
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("injectCss");
+  });
+
+});
+
+
+gulp.task('injectCss', ["styles"],function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
+});
+

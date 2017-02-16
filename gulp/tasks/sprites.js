f4d69b3aeb8601380d160750799f5e46c8090aa9
Jeff George <webdevjeffus@gmail.com>
2017-02-16 18:53:59 -0500

    Add padding to sprite file to eliminate errors in icons

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,6 +5,11 @@ var del = require("del");
 var svg2png = require("gulp-svg2png");
 
 var config = {
+  shape: {
+    spacing: {
+      padding: 1
+    }
+  },
   mode: {
     css: {
       variables: {
